de6be3e4936f9b4dacf60feb90af9a6ab00d2698
IamGauravGupta62 <gauravmodanwal62@gmail.com>
2023-09-28 15:38:38 +0530

    2nd branch commit

---
+++ src/test/java/practice/DashboardTest.java
@@ -0,0 +1,12 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class DashboardTest {
+	@Test
+	public void dashboard()
+	{
+		System.out.println("POM class is created for dashboard.");
+	}
+
+}
---
+++ src/test/java/practice/LoginTest.java
@@ -0,0 +1,12 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	@Test
+	public void login()
+	{
+		System.out.println("Login module is created.");
+	}
+
+}
